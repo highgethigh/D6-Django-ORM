15b93d21372c9f3127a6d3225f53e25cf3100326
Artem <87155286+highgethigh@users.noreply.github.com>
2023-04-27 16:16:00 +0300

    Added cache for PostList and PostDetail

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 # импортируем наше представление
 from .views import PostList, PostDetail, PostCreateView, PostDeleteView, PostUpdateView, CategoryList, CategoryDetail, add_subscribe, del_subscribe
 from . import views
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     # # т.к. сам по себе это класс, то нам надо представить этот класс в виде view.
     # Для этого вызываем метод as_view
-    path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view(), name='post'),  # pk — это первичный ключ новости, который будет выводиться у нас в шаблон
+    path('', cache_page(60*1) (PostList.as_view())), # кэшируем все новости на 1 минуту
+    path('<int:pk>', cache_page(60 * 5)(PostDetail.as_view()), name='post'),  # pk — это первичный ключ новости, который будет выводиться у нас в шаблон, кэшируем отдельную новость на 5 минут
     path('search/', views.search, name='search'),
     path('add/', PostCreateView.as_view(), name='post_create'), # ссылка на создание новости/статьи
     path('news/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
